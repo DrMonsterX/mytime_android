a05dac12bd347df2d33ea916d19d8c9fdc9b0e0a
funnygay <1316747593@qq.com>
2019-06-01 12:04:36 +0800

    修改密码功能完成

--- app/src/main/java/com/example/dy/mytime/UserPackage/UserController.java
+++ app/src/main/java/com/example/dy/mytime/UserPackage/UserController.java
@@ -12,18 +12,7 @@ public class UserController implements IGetUser{
 
     //获取目标ID用户
     public User getUser(int userId){
-        Cursor cursor=controller.searchById(userId,"User","User");
-        if(cursor.moveToNext()){
-            User user=new User(cursor.getInt(cursor.getColumnIndex("User_id")),cursor.getString(cursor.getColumnIndex("UserName")),
-                    cursor.getInt(cursor.getColumnIndex("Icon_id")),cursor.getString(cursor.getColumnIndex("Password")),
-                    cursor.getInt(cursor.getColumnIndex("Completeness_id")));
-            cursor.close();
-            controller.closeDB();
-            return user;
-        }
-        cursor.close();
-        controller.closeDB();
-        return null;
+        return LoginController.user;
     }
 
     //获取用户本周完成度
